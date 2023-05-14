183564d0319fc5aedd98ae78407ebc13e15056dc
bojanavasilevska <bojanavasilevska@gmail.com>
2023-05-14 13:53:09 +0200

    calorie-tracker-app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+import Entries from "./components/entries.component";
+
+function App() {
+  return (
+    <div className="App">
+      <Entries />
+    </div>
+  );
+}
+
+export default App;
